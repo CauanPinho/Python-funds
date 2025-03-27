3a9c71ddc8dbac756dcbd8858862809fd141894c
Cauan Pinho <Jobscauanpinho@gmail.com>
2025-03-27 11:55:04 -0400

    11:54

---
+++ RevisãoFuncoes/estudodef.py
@@ -0,0 +1,17 @@
+def mostralinha():
+    print("======================")
+
+
+
+mostralinha()
+print("sistema de transporte")
+mostralinha()
+
+mostralinha()
+print("sistema de comunicação")
+mostralinha()
+
+
+mostralinha()
+print("sistema de integração")
+mostralinha()
