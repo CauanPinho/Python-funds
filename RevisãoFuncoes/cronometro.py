c7ad9cf1199607245756368071d5045cd620ea1e
Cauan Pinho <Jobscauanpinho@gmail.com>
2025-03-28 11:02:54 -0400

    commits prontos

---
+++ RevisãoFuncoes/cronometro.py
@@ -0,0 +1,10 @@
+# Escreva uma função que recebe uma string como parâmetro e retorna o comprimento da string.
+
+# Tente agora e me envie! 🚀
+
+
+def cronometro(a):
+    while a>0:
+    print(a)
+    a=a-1
+    time.sleep()
